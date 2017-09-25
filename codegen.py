579421a41d349a084e304917e40f1b256c445a39
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-09-25 18:24:50 -0400

    Add voltage generation

--- codegen.py
+++ codegen.py
@@ -1,4 +1,3 @@
-w = 0
 class SpiceModel(object):
     def __init__(self, grid, cond_table):
         self.grid = grid
@@ -6,6 +5,7 @@ class SpiceModel(object):
         self.h= len(self.grid)
         self.w = len(self.grid[0])
         self.r_counter = 0
+        self.v_counter = 0
 
     def to_spice(self):
 
@@ -19,15 +19,50 @@ class SpiceModel(object):
 
         # generate column resistors
         self.add_block_comment("Column Resistors")
-        for i in range(self.w):
+        for i in range(self.w-1):
             self.add_comment("Column " + str(i) + " resistors")
-            for j in range(self.w-1):
+            for j in range(self.w):
                 self.add_r((i,j), (i+1,j), 1, str(self.r_counter))
                 self.r_counter += 1
 
-        # generate voltages
+        # generate row voltages
+        self.add_block_comment("Row Voltage Sources")
+        for i in range(self.w):
+            self.add_comment("Row " + str(i) + " voltage sources")
+            for j in range(self.w-1):
+                self.add_v((i,j), (i,j+1),
+                           (self.grid[i][j]-self.grid[i][j+1]),
+                           str(self.v_counter))
+                self.v_counter += 1
+
+        # generate row voltages
+        self.add_block_comment("Column Voltage Sources")
+        for i in range(self.w-1):
+            self.add_comment("Row " + str(i) + " voltage sources")
+            for j in range(self.w):
+                self.add_v((i,j), (i+1,j),
+                           (self.grid[i][j]-self.grid[i+1][j]),
+                           str(self.v_counter))
+                self.v_counter += 1
 
         # generate measurement/analysis components
+
+
+    def add_r(self, grid_idx1, grid_idx2, r, name):
+        print("R"+name,
+              self.flatten_idx(grid_idx1),
+              self.flatten_idx(grid_idx2),
+              r)
+
+    def add_v(self, grid_idx1, grid_idx2, v, name):
+        print("V"+name,
+              self.flatten_idx(grid_idx1),
+              self.flatten_idx(grid_idx2),
+              "PWL(0, "+str(v)+")")
+
+
+    def flatten_idx(self, idx):
+        return idx[0]*self.w+idx[1]
 
     def add_block_comment(self, comment):
         self.add_newline()
@@ -41,13 +76,4 @@ class SpiceModel(object):
 
     def add_comment(self, comment):
         print("* "+comment)
-
-    def add_r(self, grid_idx1, grid_idx2, r, name):
-        print("R"+name,
-              self.flatten_idx(grid_idx1),
-              self.flatten_idx(grid_idx2),
-              r)
-
-    def flatten_idx(self, idx):
-        return idx[0]*self.w+idx[1]
 
