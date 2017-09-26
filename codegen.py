b991f979dfbc5f720dbb9a30a947d952398486f5
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-09-26 15:27:44 -0400

    Get basic interpolation working

--- codegen.py
+++ codegen.py
@@ -1,11 +1,110 @@
+import numpy as np
+
 class ROCModel(object):
     def __init__(self, N):
         self.h = N
         self.w = N
         self.r_counter = 0
         self.v_counter = 0
+        self.mesh = np.zeros((self.h, self.w))
+
+    def create_mesh(self, grid):
+
+        def direct_copy(grid):
+            self.mesh = grid.copy()
+
+        def interpolate_copy(grid):
+            grid_size = len(grid) # number of cells
+            mesh_size = self.w # number of nodes
+
+            # establish grid indices and coords
+            grid_x = np.arange(0,grid_size,1)
+            grid_y = np.arange(0,grid_size,1)
+            grid_x_coords = grid_x+.5
+            grid_y_coords = grid_x+.5
+
+            # ditto for mesh
+            mesh_x = np.arange(0, mesh_size, 1)
+            mesh_y = np.arange(0, mesh_size, 1)
+
+            # calculate necessary scaling/offset constants
+            extrp_factor = float(grid_size)/float(mesh_size)
+            phys_offset = extrp_factor/2.
+            
+            # establish cell bounds
+            g_bnds = np.arange(0,grid_size+1, 1)
+            m_bnds = np.arange(0, grid_size+1, extrp_factor)
+
+            # for a given cell return it's bounds
+            def cell_to_bound(i,j,bounds):
+               return (bounds[j+1], bounds[i], bounds[j] , bounds[i+1]) 
+
+            # intesect two bounds objects. any negativity means
+            # rectangles don't have an intersection
+            def intersect(b1, b2):
+                return (min(b1[0],b2[0]), max(b1[1],b2[1]),
+                        max(b1[2],b2[2]), min(b1[3],b2[3]))
+
+            # calcute area of an intersection
+            def area(b):
+                if b[0]-b[2] < 0 or b[3]-b[1] < 0:
+                    return 0.
+                return (b[0]-b[2])*(b[3]-b[1])
+
+            # iterate nearest mesh cell indices for a given grid index
+            def nearest_mesh_cells(g_i, g_j):
+
+                def __nmc(g_io, g_jo):
+                    yield (int(np.floor(g_io/extrp_factor))-1,
+                           int(np.floor(g_jo/extrp_factor))-1)
+                    yield (int(np.floor(g_io/extrp_factor))-1,
+                           int(np.floor(g_jo/extrp_factor))  )
+                    yield (int(np.floor(g_io/extrp_factor))  ,
+                           int(np.floor(g_jo/extrp_factor))-1)
+                    yield (int(np.floor(g_io/extrp_factor))  ,
+                           int(np.floor(g_jo/extrp_factor))  )
+
+                g_io = g_i+phys_offset
+                g_jo = g_j+phys_offset
+
+                for tmp_cell in __nmc(g_io,g_jo):
+                    if tmp_cell[0] >= 0 and tmp_cell[0] < mesh_size:
+                        if tmp_cell[1] >= 0 and tmp_cell[1] < mesh_size:
+                            yield tmp_cell
+
+            # iterate over the grid and calculate weighted sums
+            for g_i in grid_x:
+                for g_j in grid_y:
+                    for (m_i, m_j) in nearest_mesh_cells(g_i, g_j):
+                        self.mesh[m_i][m_j] += grid[g_i][g_j] * area(intersect(cell_to_bound(g_i,g_j,g_bnds),
+                                           cell_to_bound(m_i,m_j,m_bnds)))
+
+
+            # average the mesh
+            # self.mesh = [m/(extrp_factor**2) for m in self.mesh]
+            self.mesh = self.mesh/(extrp_factor**2)
+            # print(extrp_factor)
+            print(grid)
+            print(self.mesh)
+        #
+        # end of interpolate
+        #
+        
+        grid_h = len(grid)
+        grid_w = len(grid[0])
+
+        if grid_w <= self.w:
+            direct_copy(grid)
+        else: # problem is larger than the mesh
+            interpolate_copy(grid)
+    #
+    # end of create_mesh
+    #
 
     def to_spice(self, grid, conductance):
+
+        print(grid)
+        self.create_mesh(grid)
 
         # generate row resistors
         self.add_block_comment("Row Resistors")
@@ -29,7 +128,7 @@ class ROCModel(object):
             self.add_comment("Row " + str(i) + " voltage sources")
             for j in range(self.w-1):
                 self.add_v((i,j), (i,j+1),
-                           (grid[i][j]-grid[i][j+1]),
+                           (self.mesh[i][j]-self.mesh[i][j+1]),
                            str(self.v_counter))
                 self.v_counter += 1
 
@@ -39,7 +138,7 @@ class ROCModel(object):
             self.add_comment("Row " + str(i) + " voltage sources")
             for j in range(self.w):
                 self.add_v((i,j), (i+1,j),
-                           (grid[i][j]-grid[i+1][j]),
+                           (self.mesh[i][j]-self.mesh[i+1][j]),
                            str(self.v_counter))
                 self.v_counter += 1
 
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 #! /usr/bin/env python
+import numpy as np
 from codegen import *
 
 # I am using python 3.6.1
@@ -7,15 +8,15 @@ from codegen import *
 N = 4
 
 # initialize grid
-grid = [[0. for i in range(N)] for j in range(N)]
+grid = np.zeros((2*N,2*N))
 
 # heat up an arbitrary point
-grid[2][3] = 1.
+grid[2][2] = 1.
 
 # initialize conductance table
 # conductance = [[1. for i in range(N-1)] for j in range(N-1)]
 conductance = 1.
 
 # print(grid)
-m = ROCModel(N)
+m = ROCModel(4)
 m.to_spice(grid, conductance)
