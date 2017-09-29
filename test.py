1b03401b269bfbfee03801c066b2e17aec63f6f6
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-09-29 16:37:50 -0400

    Parametrize the test file

--- test.py
+++ test.py
@@ -1,28 +1,41 @@
 #! /usr/bin/env python
+import sys
 import numpy as np
 from codegen import *
+import matplotlib.pyplot as plt
 
 # I am using python 3.6.1
 
 # assume square grid
-N = 10
+N = 100
+mesh_size = int(sys.argv[1])
+img_name = 'hm_{gr_sz}_{ms_sz}'
 
 # initialize grid
 grid = np.zeros((N,N))
 
 # heat up an arbitrary point
-grid[1][1] = 10.
+grid[5][5] = 10.
 
-# heat up in gradient from the right
-# for (i,j) in it.product(range(N), range(N)):
-    # grid[i][j] = j*0.5
-
-print(grid)
-# initialize conductance table
-# conductance = [[1. for i in range(N-1)] for j in range(N-1)]
-conductance = .1
+cond_exp = -1
+conductance = 10**cond_exp
 
 # print(grid)
-m = ROCModel(10)
+m = ROCModel(mesh_size)
 m.load_problem(grid, conductance)
-print(m.run_spice_solver())
+result = m.run_spice_solver()
+
+debug = False
+if debug:
+    for i in range(N):
+        for j in range(N):
+            print('{0: >11.5f} '.format((result[i][j])*10.**15), end='')
+        print()
+
+# save the input grid as well
+# plt.imshow(grid, cmap='hot', interpolation='nearest')
+# plt.savefig('tmp')
+
+plt.imshow(result, cmap='hot', interpolation='nearest')
+plt.savefig(img_name.format(gr_sz=N, ms_sz=mesh_size))
+
