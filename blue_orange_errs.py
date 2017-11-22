5e21b3ac0e7d05103ba299784872331bcc62110f
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-11-22 15:35:05 -0500

    Cosmetic changes in blue orange error test

--- blue_orange_errs.py
+++ blue_orange_errs.py
@@ -11,7 +11,7 @@ from analysis_utils import (aggregate_current_vectors,
                             energy_flow,
                             plot_heatmap)
 
-exp_largest_mesh = 4
+exp_largest_mesh = 6
 prob_size = 2**exp_largest_mesh
 source = (0, 0, 1, 1)
 sink = (prob_size-1, prob_size-1, 1, 1)
@@ -19,10 +19,9 @@ cond_exp = -3
 conductance = 10**cond_exp
 hp = HeatProblem(prob_size, source, sink, conductance, src_val=10.)
 
-use_cached = False
+use_cached = True
 
 filename='tmp/bo_err_{}'
-
 ground_truth_mesh = ROCModel(2**exp_largest_mesh)
 exp_size_range = range(2,exp_largest_mesh)
 meshes = {exp_size:ROCModel(2**exp_size) for exp_size in exp_size_range}
@@ -64,13 +63,39 @@ for exp_mesh_size, mesh in meshes.items():
     blue_errs.append(abs(tmp_grid[tmp_bp]-base_grid[tmp_bp]))
     orange_errs.append(abs(tmp_grid[tmp_op]-base_grid[tmp_op]))
 
+
+rect = 0.1,0.2,0.8,0.7
+fig = plt.figure(figsize=(10,5))
+ax = fig.add_axes(rect)
+
 sizes = [2**i for i in exp_size_range]
-print(blue_errs)
-print(orange_errs)
-fig, ax = plt.subplots(1,1)
-ax.plot(sizes, blue_errs, sizes, orange_errs)
-ax.set_xticks(sizes)
-ax.set_xticklabels(sizes)
-ax.set_ylim((0, max(max(blue_errs), max(orange_errs))*1.1))
-ax.set_xlim(0,2**(exp_largest_mesh-1)*1.1)
+
+def custom_plot(ax, *xy):
+    ax.plot(*xy, marker='o',
+                 markerfacecolor='none',
+                 markeredgewidth=2)
+
+    ax.set_xlabel('Mesh Size')
+    ax.set_xticks(sizes)
+    ax.set_xticklabels(sizes)
+
+    ax.set_ylabel('Error')
+
+    ax.set_ylim((0, max([max(xy[i]) for i in range(1,len(xy),2)])*1.1))
+    ax.set_xlim(0,prob_size/2+1)
+
+    ax.grid(b=True, axis='x')
+    ax.grid(b=True, axis='y', linestyle='dashed')
+
+    ax.spines['top'].set_linewidth(1)
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['left'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(1)
+
+custom_plot(ax, sizes, blue_errs, sizes, orange_errs)
+# ax.plot(sizes, blue_errs, sizes, orange_errs)
+# ax.set_xticks(sizes)
+# ax.set_xticklabels(sizes)
+# ax.set_ylim((0, max(max(blue_errs), max(orange_errs))*1.1))
+# ax.set_xlim(0,2**(exp_largest_mesh-1)*1.1)
 plt.show()
