cf0f2bc0d11db4a1b4d2672612104a6e2742ab4a
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-11-22 15:48:03 -0500

    Add the current test file

---
+++ current_vs_v.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python
+from roc_model import ROCModel
+from heat_problem import HeatProblem
+from analysis_utils import print_current_table
+
+exp_prob_size = 5
+prob_size = 2**exp_prob_size
+source = (0, 0, 1, 1)
+sink = (prob_size-1, prob_size-1, 1, 1)
+exp_cond = -3
+conductance = 10**exp_cond
+
+filename='tmp/diag_v_{}'
+
+mesh = ROCModel(prob_size)
+
+def get_results(mesh, problem):
+    mesh.load_problem(problem)
+    mesh.run_spice_solver(f)
+    return mesh.final_grid
+
+for v in range(1,5):
+    print('Source potential = {} V'.format(v))
+    hp = HeatProblem(prob_size, source, sink, conductance, src_val=v)
+    res_grid = get_results(mesh, hp, cached=use_cached)
+    print_current_table(mesh)
+    print()
