7a7ae8c0b77457e4d57ac3b92699af0a9f6bb88e
Engin Kayraklioglu <ekayrakli@gmail.com>
2017-12-06 16:11:18 -0500

    Add initial implementation of cross-section analysis

---
+++ test/comsol_comparison/comsol_csect_comp.py
@@ -0,0 +1,105 @@
+#! /usr/bin/env python
+import sys
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from roc_model import ROCModel
+from heat_problem import HeatProblem
+from analysis_utils import *
+
+
+# in COMSOL I still can't create a 2D region that is to be taken as
+# fixed temperature in our case. Which may not make sense with
+# physical/realistic use cases, anyway.
+# So, here we are creating a heat problem that includes source and sink
+# regions, but then when comparing against comsol data, we need to get
+# the center slice which doesn't have any source or sink within.
+# so we need to create a heat problem larger than the comsol data, so
+# that we can get the center slice that has the same data resolution
+def main():
+    size = int(sys.argv[1])
+    mesh_size = int(sys.argv[2])
+
+    comp_size = int(size/(mesh_size-2))
+    if comp_size != size/(mesh_size-2):
+        print('Divisibility error')
+
+    prb_size = size+2*comp_size  # one component on each side
+
+    source = (0, 0, comp_size, prb_size)
+    sink = [(comp_size, 0, prb_size-comp_size, comp_size),
+            (prb_size-comp_size, 0, comp_size, prb_size),
+            (comp_size, prb_size-comp_size, prb_size-comp_size, comp_size)]
+
+
+    conductance = 10**-3  # this'll be used as resistance directly
+    hp = HeatProblem(prb_size, source, sink, conductance, src_val=1.)
+
+    m = ROCModel(mesh_size)
+    m.load_problem(hp)
+    m.run_spice_solver(filename='problem')
+
+    from_mesh = m.final_grid[comp_size:comp_size+size,
+                             comp_size:comp_size+size]
+    from_comsol = load_grid_from_comsol_csv('comsol_data/left_src_others_sink_comsol.csv')
+
+
+    mesh_sect = get_isect(from_mesh, 150)
+    comsol_sect = get_isect(from_comsol, 150)
+
+
+    rect = 0.1,0.2,0.8,0.7
+    fig = plt.figure(figsize=(15,5))
+    ax = fig.add_axes(rect)
+
+    init_plot(ax, x_lim=size,
+                  y_lim=max(max(mesh_sect), max(comsol_sect)))
+    custom_plot(ax, range(0,300), mesh_sect, label='Electrical Mesh')
+    custom_plot(ax, range(0,300), comsol_sect, label='Comsol')
+    do_plots()
+
+
+
+# end of main
+
+def get_isect(data, idx):
+    i_len = data.shape[0]
+    return [r[0] for r in data[0:i_len, idx:idx+1]]
+
+datasets = []
+def custom_plot(ax, x, y, label=''):
+    handle, = ax.plot(x, y, label=label,
+                            marker='o',
+                            markerfacecolor='none',
+                            markeredgewidth=2)
+
+    datasets.append(handle)
+
+def init_plot(ax, x_lim, y_lim):
+    num_x_ticks=10
+    # put only 8 ticks on x axis
+    ticks = [i for i in range(0, x_lim, int(x_lim/num_x_ticks))]
+
+    ax.set_xlabel('Grid point')
+    ax.set_xticks(ticks)
+    ax.set_xticklabels(ticks)
+
+    ax.set_ylabel('Potential/Temperature')
+
+    ax.set_ylim((0, y_lim*1.1))
+    ax.set_xlim(0, x_lim)
+
+    ax.grid(b=True, axis='x')
+    ax.grid(b=True, axis='y', linestyle='dashed')
+
+    ax.spines['top'].set_linewidth(1)
+    ax.spines['bottom'].set_linewidth(1)
+    ax.spines['left'].set_linewidth(1)
+    ax.spines['right'].set_linewidth(1)
+
+def do_plots():
+    plt.legend(handles=datasets)
+    plt.show()
+    
+
+if __name__=='__main__':
+    main()
